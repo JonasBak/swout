15c491431e6477c0b573bae8ce5185bd953370e7
Jonas Bakken <jonasxb@gmail.com>
2019-12-07 21:12:45 +0100

    Draw name of output

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,27 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
+use sdl2::render::Texture;
+use sdl2::render::TextureCreator;
+use sdl2::render::TextureQuery;
+use sdl2::ttf::Font;
+use sdl2::video::WindowContext;
 
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
 use std::process::Command;
 use std::thread::sleep;
 use std::time::Duration;
+
+// TODO fix all pos duplicate code
+// TODO make it not ugly
+// TODO make text not ugly
+// TODO "export" layout
+
+struct NameTexture<'a> {
+    format: f32,
+    texture: Texture<'a>,
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Bounds {
@@ -18,16 +33,16 @@ struct Bounds {
     height: u32,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Output {
+struct Output<'a> {
     id: u16,
     name: String,
     rect: Bounds,
+    name_texture: NameTexture<'a>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct InactiveOutput {
+struct InactiveOutput<'a> {
     name: String,
+    name_texture: NameTexture<'a>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -45,7 +60,10 @@ struct MouseTracker {
     size: (u32, u32),
 }
 
-fn get_outputs() -> Result<(Vec<Output>, Vec<InactiveOutput>)> {
+fn get_outputs<'a>(
+    font: &Font,
+    texture_creator: &'a TextureCreator<WindowContext>,
+) -> Result<(Vec<Output<'a>>, Vec<InactiveOutput<'a>>)> {
     let raw_output = Command::new("swaymsg")
         .arg("-t")
         .arg("get_outputs")
@@ -55,37 +73,71 @@ fn get_outputs() -> Result<(Vec<Output>, Vec<InactiveOutput>)> {
     let mut inactive_outputs: Vec<InactiveOutput> = Vec::new();
     let outputs: Vec<TmpOutput> = serde_json::from_slice(&raw_output.stdout)?;
     for output in outputs.into_iter() {
+        let surface = font
+            .render(&output.name)
+            .blended(Color::RGBA(0, 0, 0, 255))
+            .map_err(|e| e.to_string())
+            .unwrap();
+        let texture = texture_creator
+            .create_texture_from_surface(&surface)
+            .map_err(|e| e.to_string())
+            .unwrap();
+        let TextureQuery { width, height, .. } = texture.query();
+
         if let Some(id) = output.id {
             active_outputs.push(Output {
                 id,
                 name: output.name,
                 rect: output.rect,
+                name_texture: NameTexture {
+                    format: width as f32 / height as f32,
+                    texture,
+                },
             });
         } else {
-            inactive_outputs.push(InactiveOutput { name: output.name });
+            inactive_outputs.push(InactiveOutput {
+                name: output.name,
+                name_texture: NameTexture {
+                    format: width as f32 / height as f32,
+                    texture,
+                },
+            });
         }
     }
 
     return Ok((active_outputs, inactive_outputs));
 }
 
-fn set_active(output: &InactiveOutput) -> (Vec<Output>, Vec<InactiveOutput>) {
+fn set_active<'a>(
+    output: &InactiveOutput,
+    font: &Font,
+    texture_creator: &'a TextureCreator<WindowContext>,
+) -> (Vec<Output<'a>>, Vec<InactiveOutput<'a>>) {
     Command::new("swaymsg")
         .arg(format!("output {} enable", output.name))
         .output()
         .expect("failed to get outputs from swaymsg");
-    get_outputs().unwrap()
+    get_outputs(font, texture_creator).unwrap()
 }
 
-fn set_inactive(output: &Output) -> (Vec<Output>, Vec<InactiveOutput>) {
+fn set_inactive<'a>(
+    output: &Output,
+    font: &Font,
+    texture_creator: &'a TextureCreator<WindowContext>,
+) -> (Vec<Output<'a>>, Vec<InactiveOutput<'a>>) {
     Command::new("swaymsg")
         .arg(format!("output {} disable", output.name))
         .output()
         .expect("failed to get outputs from swaymsg");
-    get_outputs().unwrap()
+    get_outputs(font, texture_creator).unwrap()
 }
 
-fn update_output_position(moved: MouseTracker, mut outputs: Vec<Output>) -> Vec<Output> {
+fn update_output_position<'a>(
+    moved: MouseTracker,
+    mut outputs: Vec<Output>,
+    font: &Font,
+    texture_creator: &'a TextureCreator<WindowContext>,
+) -> Vec<Output<'a>> {
     let mut min_x = moved.pos.0;
     let mut min_y = moved.pos.1;
     for output in outputs.iter_mut() {
@@ -116,7 +168,7 @@ fn update_output_position(moved: MouseTracker, mut outputs: Vec<Output>) -> Vec<
         .output()
         .expect("failed to set outputs");
 
-    get_outputs().unwrap().0
+    get_outputs(font, texture_creator).unwrap().0
 }
 
 fn check_inside(point: (i32, i32), bounds: &Bounds) -> bool {
@@ -124,7 +176,7 @@ fn check_inside(point: (i32, i32), bounds: &Bounds) -> bool {
         && (bounds.y..=bounds.y + bounds.height as i32).contains(&point.1)
 }
 
-fn check_touched(point: (i32, i32), outputs: &[Output]) -> Option<&Output> {
+fn check_touched<'a>(point: (i32, i32), outputs: &'a [Output]) -> Option<&'a Output<'a>> {
     for output in outputs.iter() {
         if check_inside(point, &output.rect) {
             return Some(output);
@@ -133,11 +185,11 @@ fn check_touched(point: (i32, i32), outputs: &[Output]) -> Option<&Output> {
     None
 }
 
-fn check_inactive_touched(
+fn check_inactive_touched<'a>(
     point: (i32, i32),
     inactive_size: u32,
-    outputs: &[InactiveOutput],
-) -> Option<&InactiveOutput> {
+    outputs: &'a [InactiveOutput],
+) -> Option<&'a InactiveOutput<'a>> {
     for (i, output) in outputs.iter().enumerate() {
         if check_inside(
             point,
@@ -204,11 +256,9 @@ fn handle_overlap(mut moved: MouseTracker, outputs: &[Output]) -> MouseTracker {
 }
 
 fn main() {
-    let (mut active_outputs, mut inactive_outputs) = get_outputs().unwrap();
-
     let scale = 10.0;
 
-    let inactive_size = 50 * scale as u32;
+    let inactive_size = 80 * scale as u32;
 
     let colors = vec![(255, 0, 0), (0, 255, 0), (0, 0, 255)];
     let selected_color = (200, 200, 200);
@@ -217,7 +267,7 @@ fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
     let window = video_subsystem
-        .window("rust-sdl2 demo: Video", 800, 600)
+        .window("swout", 800, 600)
         .position_centered()
         .opengl()
         .resizable()
@@ -230,10 +280,21 @@ fn main() {
         .build()
         .map_err(|e| e.to_string())
         .unwrap();
+    let texture_creator = canvas.texture_creator();
 
     canvas.set_scale(1.0 / scale, 1.0 / scale).unwrap();
 
+    let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string()).unwrap();
+    let font = ttf_context
+        .load_font(
+            "/usr/share/fonts/liberation-sans/LiberationSans-Regular.ttf",
+            200,
+        )
+        .unwrap();
+
     let mut event_pump = sdl_context.event_pump().unwrap();
+
+    let (mut active_outputs, mut inactive_outputs) = get_outputs(&font, &texture_creator).unwrap();
 
     let mut selected: Option<MouseTracker> = None;
 
@@ -279,25 +340,39 @@ fn main() {
                 _ => colors[i],
             };
             canvas.set_draw_color(Color::RGB(color.0, color.1, color.2));
+            let mut rect = Rect::new(
+                output.rect.x + center.0,
+                output.rect.y + center.1,
+                output.rect.width,
+                output.rect.height,
+            );
+            canvas.fill_rect(rect).unwrap();
+            if (rect.width() as f32 / rect.height() as f32) < output.name_texture.format {
+                rect.set_height((rect.width() as f32 / output.name_texture.format) as u32);
+            } else {
+                rect.set_width((output.name_texture.format * rect.height() as f32) as u32);
+            }
             canvas
-                .fill_rect(Rect::new(
-                    output.rect.x + center.0,
-                    output.rect.y + center.1,
-                    output.rect.width,
-                    output.rect.height,
-                ))
+                .copy(&output.name_texture.texture, None, Some(rect))
                 .unwrap();
         }
         // Render inactive
-        for (i, _) in inactive_outputs.iter().enumerate() {
-            canvas.set_draw_color(Color::RGB(100, 0, 100));
+        for (i, output) in inactive_outputs.iter().enumerate() {
+            canvas.set_draw_color(Color::RGB(200, 200, 200));
+            let mut rect = Rect::new(
+                inactive_size as i32 * i as i32,
+                0,
+                inactive_size as u32,
+                inactive_size as u32,
+            );
+            canvas.fill_rect(rect).unwrap();
+            if (rect.width() as f32 / rect.height() as f32) < output.name_texture.format {
+                rect.set_height((rect.width() as f32 / output.name_texture.format) as u32);
+            } else {
+                rect.set_width((output.name_texture.format * rect.height() as f32) as u32);
+            }
             canvas
-                .fill_rect(Rect::new(
-                    inactive_size as i32 * i as i32,
-                    0,
-                    inactive_size as u32,
-                    inactive_size as u32,
-                ))
+                .copy(&output.name_texture.texture, None, Some(rect))
                 .unwrap();
         }
         canvas.present();
@@ -331,13 +406,14 @@ fn main() {
                             inactive_size,
                             &inactive_outputs,
                         ) {
-                            let (new_active_outputs, new_inactive_outputs) = set_active(&output);
+                            let (new_active_outputs, new_inactive_outputs) =
+                                set_active(&output, &font, &texture_creator);
                             active_outputs = new_active_outputs;
                             inactive_outputs = new_inactive_outputs;
                         } else if active_outputs.len() > 1 {
                             if let Some(output) = check_touched((x, y), &active_outputs) {
                                 let (new_active_outputs, new_inactive_outputs) =
-                                    set_inactive(&output);
+                                    set_inactive(&output, &font, &texture_creator);
                                 active_outputs = new_active_outputs;
                                 inactive_outputs = new_inactive_outputs;
                             }
@@ -356,7 +432,8 @@ fn main() {
                 }
                 Event::MouseButtonUp { .. } => {
                     if let Some(moved) = selected.take() {
-                        active_outputs = update_output_position(moved, active_outputs);
+                        active_outputs =
+                            update_output_position(moved, active_outputs, &font, &texture_creator);
                     }
                 }
                 Event::MouseMotion {
